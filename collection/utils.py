96c2613af944c4dac9c538ced63f11f601148a5c
gahan9 <gahansaraiya@gmail.com>
2018-10-30 00:00:20 +0530

    modification for data dump

--- collection/utils.py
+++ collection/utils.py
@@ -1,15 +1,24 @@
 import os
+import sys
+from threading import Thread
+from itertools import islice
+
+from django.db import DataError
+
 from collection.models import Title
 from moviedb.settings import BASE_DIR
 
+DATA_LEN = 3671031
 
-def add_data():
+
+def generate_data():
     FILE_PATH = os.path.join(BASE_DIR, "dump", "data.tsv")
     if os.path.exists(FILE_PATH):
         with open(FILE_PATH, 'r', encoding="utf-8") as f:
             content = f.read()
         data = content.split("\n")
-        for val in data[1000:]:
+        DATA_RANGE_SKIP = Title.objects.count()
+        for val in data[DATA_RANGE_SKIP:]:
             row = [i.replace("\\N", "").strip() if i.replace("\\N", "").strip() else None for i in val.split("\t")]
             # print(row)
             # try:
@@ -26,11 +35,25 @@ def add_data():
                 "attributes": row[6],
                 "is_original_title": bool(row[7]),
             }
+            return row_dict
             # print(row_dict)
-            Title.objects.get_or_create(**row_dict)
+            # try:
+            #     Title.objects.get_or_create(**row_dict)
+            # except DataError:
+            #     print("Error for below entry:\n{}".format(row_dict))
     else:
         print("Path not exist")
 
 
+def add_data_to_db(batch_size=500):
+    DATA_RANGE = DATA_LEN - Title.objects.count()
+    objects = (Title(generate_data()) for i in range(min(1000, DATA_RANGE)))
+    while True:
+        batch = list(islice(objects, batch_size))
+        if not batch:
+            break
+        Title.objects.bulk_create(batch, batch_size)
+
+
 if __name__ == "__main__":
-    add_data()
+    add_data_to_db()
